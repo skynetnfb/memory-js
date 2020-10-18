6bf01ebdd339e93dabb16aeaa1592e0dd57413ba
skynetnfb <46841502+skynetnfb@users.noreply.github.com>
2020-10-18 09:56:06 +0200

    board funzionante bug risolto punteggi da implementare

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import {utils} from './utils';
-import './styles/main.scss'
-console.log('hello world!!!!');
+import './styles/main.scss';
 
 
+/////////////////CARTE DA GIOCO /////////////////
+var memory_array = [
+    './src/images/1.png','./src/images/1.png',
+    './src/images/2.png','./src/images/2.png',
+    './src/images/3.png','./src/images/3.png',
+    './src/images/4.png','./src/images/4.png',
+    './src/images/5.png','./src/images/5.png',
+    './src/images/6.png','./src/images/6.png',
+    './src/images/7.png','./src/images/7.png',
+    './src/images/8.png','./src/images/8.png'
+];
 
-
-var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H'];
-var memory_values=[];
-var memory_tile_ids=[];
-var  tiles_flipped=0;
+//var memory_values=[];
+//var memory_tile_ids=[];
 var first_card;
 var second_card;
+var stop;
 
 var actual_score_value=0;
 var firstFlippedCard=null;
 
 
-var game = {
+var partita = {
     score: 0,
     errors: 0,
 };
 
 
-var score_title= document.createElement('h1');
+
 var memory_board=document.createElement("div");
 var scores= document.createElement('span');
 const board=document.getElementById("board");
@@ -32,12 +40,62 @@ console.log(memory_board.getAttribute("id"));
 console.log(board);
 
 scores.setAttribute('id','scores');
-score_title.innerText='scores';
-scores.appendChild(score_title);
+//var score_title= document.createElement('h1');
+//score_title.innerText='scores';
+//scores.appendChild(score_title);
 board.appendChild(scores);
 
+var panel_score= document.createElement('span');
+panel_score.setAttribute("class","panel");
+var panel_error= document.createElement('span');
+panel_error.setAttribute("class","panel");
+var panel_score_text= document.createElement('h1');
+var panel_error_text= document.createElement('h1');
+panel_score_text.innerText='Punteggio';
+panel_error_text.innerText='Errori';
+panel_score.appendChild(panel_score_text);
+panel_error.appendChild(panel_error_text);
+scores.appendChild(panel_score);
+scores.appendChild(panel_error);
+var value_score_text= document.createElement('h2');
+var value_error_text= document.createElement('h2');
+value_score_text.innerText=partita.score;
+value_error_text.innerText=partita.errors;
+panel_score.appendChild(value_score_text);
+panel_error.appendChild(value_error_text);
 
-Array.prototype.memory_tile_shuffle= function () {
+var panel_button= document.createElement('span');
+panel_button.setAttribute("class","panel");
+var panel_button_text= document.createElement('h1');
+panel_button_text.innerText='prova';
+panel_button.appendChild(panel_button_text);
+
+//
+
+var panel_timer= document.createElement('span');
+panel_timer.setAttribute("class","panel");
+var panel_timer_text= document.createElement('h1');
+panel_timer_text.innerText='Timer';
+panel_timer.appendChild(panel_timer_text);
+var value_timer_text= document.createElement('h2');
+value_timer_text.innerText="0";
+panel_timer.appendChild(value_timer_text);
+scores.appendChild(panel_timer);
+scores.appendChild(panel_button);
+/*
+
+
+
+panel_timer.appendChild(panel_timer_text);
+
+*/
+/*
+
+*/
+
+
+
+Array.prototype.memory_card_shuffle= function () {
     var i = this.length, j, temp;
     while(i-->0){
         j=Math.floor(Math.random()* (i+1));
@@ -49,26 +107,32 @@ Array.prototype.memory_tile_shuffle= function () {
 }
 function newBoard(){
     ////////////////////////TIMER///////////////////////////
-    var timer= document.createElement('div');
-    timer.setAttribute('id','timer');
-    scores.appendChild(timer);
-    var distance = 1000;
+    //var timer= document.createElement('div');
+    //timer.setAttribute('id','timer');
+    //scores.appendChild(timer);
+    var distance = 300;
     var x = setInterval(function() {
         var seconds = --distance ;
         console.log( distance );
-        document.getElementById('timer').innerHTML = seconds + "s ";
+        //document.getElementById('timer').innerHTML = seconds + "s ";
+        value_timer_text.innerText=seconds + "s";
         if (seconds < 0) {
             clearInterval(x);
             document.getElementById("timer").innerHTML = 'Game Over';
                 gameOver();
         }
-    }, 10000);
+    }, 1000);
+
     /////////////////////////////////////////////////
 
-    tiles_flipped = 0;
-    var output = '';
-    memory_array.memory_tile_shuffle();
-    console.log(memory_array);
+    //si mischiano gli elementi dell'array con la funzione aggiunta al prototype dell'array memory_card_shuffle
+    //viene creato un elemento del dom card per ogni elemento dell'array
+    // l'indice viene salvato nell'attributo name della carta
+    //si aggiunge l'vento flip ad ogni carta
+    //ogni elemento viene poi appeso al tavolo da gioco
+
+    memory_array.memory_card_shuffle();
+
     for(var i = 0; i < memory_array.length; i++){
         let card= document.createElement('div');
         card.setAttribute('id','card'+i);
@@ -79,51 +143,82 @@ function newBoard(){
 }
 }
 function flip() {
-    console.log('card flipped:'+this.id);
-    console.log(this);
-    function flip_first(current_card) {
-        first_card=current_card;
-        console.log(current_card.getAttribute('name'));
-        let cardContent = document.createElement('p');
-        //cardContent.innerText = current_card.getAttribute('name');
-        cardContent.innerText = memory_array[current_card.getAttribute('name')];
-        current_card.appendChild(cardContent);
-        current_card.setAttribute("style", "background-color: red;");
-        firstFlippedCard=current_card.getAttribute('name')}
+    console.log(stop);
+    if(stop== undefined){
+    if (first_card == undefined) {
+        first_card = this;
+        console.log('dentro if primo flip:' + first_card.getAttribute('name'));
+        console.log(this);
+        console.log(first_card);
+        flip_card(first_card);
+    } else if (first_card != undefined && second_card == undefined) {
+        second_card = this;
+        console.log('dentro if secondo flip:' + first_card.getAttribute('name'));
+        console.log(this);
+        console.log(second_card);
+        flip_card(second_card);
 
-    if(firstFlippedCard==null){
-        flip_first(this);
-
-    }else
-
-        var cardContent = document.createElement('span');
-        cardContent.innerText = memory_array[this.getAttribute('name')];
+        /*
+        var cardContent = document.createElement('img');
+        cardContent.setAttribute("src",memory_array[this.getAttribute('name')]);
         this.appendChild(cardContent);
-        second_card=this;
-        second_card.setAttribute("style", "background-color: red;");
-        console.log(memory_array[this.getAttribute('name')]);
-
-        if(memory_array[first_card.getAttribute('name')]==memory_array[this.getAttribute('name')]){
-            first_card.setAttribute("style","background-color: green;");
-            this.setAttribute("style","background-color: green;");
-            firstFlippedCard=null;
-            first_card=null;
-            second_card=null;
-    actual_score_value=++actual_score_value;
-    console.log('score:'+actual_score_value);
-    }else {
-            console.log('primo valore:'+first_card.getAttribute('name'));
-            console.log('secondo valore:'+this.getAttribute('name'));
+        this.setAttribute("style", "background-color: red;");
+        second_card=this;*/
+    }
+    if (first_card != undefined && second_card != undefined) {
+        console.log('dentro if check');
+        console.log(this);
+        console.log(second_card);
+        console.log(first_card);
+        console.log('check prima carta:' + first_card.getAttribute('name'));
+        console.log('check seconda carta:' + second_card.getAttribute('name'));
+        if (memory_array[first_card.getAttribute('name')] == memory_array[second_card.getAttribute('name')]) {
+            first_card.setAttribute("style", "background-color: green;");
+            second_card.setAttribute("style", "background-color: green;");
+            //firstFlippedCard = null;
+            first_card = undefined;
+            second_card = undefined;
+            stop=undefined;
+            actual_score_value = ++actual_score_value;
+            partita.score = ++partita.score;
+            value_score_text.innerText=partita.score;
+            console.log('score:' + actual_score_value);
+            console.log('partita score:' + partita.score);
+        } else {
+            console.log('primo valore:' + first_card.getAttribute('name'));
+            console.log('secondo valore:' + this.getAttribute('name'));
             setTimeout(() => {
                 first_card.removeChild(first_card.lastElementChild);
-                first_card.setAttribute("style","background-color: #FFD02A;");
-                this.setAttribute("style","background-color: #FFD02A;");
+                first_card.setAttribute("style", "background-color: #FFD02A;");
                 this.removeChild(second_card.lastElementChild);
-                firstFlippedCard=null;
-                first_card=null;
-                second_card=null;
+                this.setAttribute("style", "background-color: #FFD02A;");
+                //firstFlippedCard = null;
+                first_card = undefined;
+                second_card = undefined;
+                stop=undefined;
+                partita.errors=++partita.errors;
+                value_error_text.innerText=partita.errors;
+                console.log('errori:'+ partita.errors)
             }, 1000);
         }
+
+    }
+}
+
+
+
+    function flip_card(current_card) {
+        console.log(current_card.getAttribute('name'));
+        let cardContent = document.createElement('img');
+        cardContent.setAttribute("src", memory_array[current_card.getAttribute('name')]);
+        current_card.appendChild(cardContent);
+        current_card.setAttribute("style", "background-color: red;");
+        if(second_card!=undefined)stop=1;
+    }
+
+    function check_card(){
+
+    }
 
 }
 
@@ -131,48 +226,6 @@ function gameOver(){
     board.removeChild(memory_board)
 };
 
-/*
-function memoryFlipTile(tile,val){
-    console.log("qui");
-    if(tile.innerHTML == "" && memory_values.length < 2){
-        tile.style.background = '#FFF';
-        tile.innerHTML = val;
-        if(memory_values.length == 0){
-            memory_values.push(val);
-            memory_tile_ids.push(tile.id);
-        } else if(memory_values.length == 1){
-            memory_values.push(val);
-            memory_tile_ids.push(tile.id);
-            if(memory_values[0] == memory_values[1]){
-                tiles_flipped += 2;
-                // Clear both arrays
-                memory_values = [];
-                memory_tile_ids = [];
-                // Check to see if the whole board is cleared
-                if(tiles_flipped == memory_array.length){
-                    alert("Board cleared... generating new board");
-                    document.getElementById('memory_board').innerHTML = "";
-                    newBoard();
-                }
-            } else {
-                console.log("qui2");
-                function flip2Back(){
-                    // Flip the 2 tiles back over
-                    var tile_1 = document.getElementById(memory_tile_ids[0]);
-                    var tile_2 = document.getElementById(memory_tile_ids[1]);
-                    tile_1.style.background = 'url(tile_bg.jpg) no-repeat';
-                    tile_1.innerHTML = "";
-                    tile_2.style.background = 'url(tile_bg.jpg) no-repeat';
-                    tile_2.innerHTML = "";
-                    // Clear both arrays
-                    memory_values = [];
-                    memory_tile_ids = [];
-                }
-                setTimeout(flip2Back, 700);
-            }
-        }
-    }
-}*/
 
 newBoard();
 
